3ab9adae5a06e2a7eee6939416926b801585acd9
wellupweb <wellupweb@gmail.com>
2023-06-22 16:17:51 +0600

    helo

---
+++ javascript/index.js
@@ -0,0 +1,47 @@
+// let carname = "Toyota"; // string
+// let age = 30; //number
+// let color = undefined;
+// let brandname = null; 
+
+// console.log(carname); 
+// console.log(age);
+// console.log(color);
+// console.log(brandname);
+
+
+// OBJECTS
+
+let tower = {
+    color: "green",
+    size: 5,
+    flatNumber : 20,
+    room: {
+        color: "red",
+        size: 220
+    }
+}
+
+// Access Object By Dot Notation
+// console.log(tower.room.size);
+
+// Access Object by Index
+// let indexOfObject = "color";
+// console.log(tower["room"]["color"]);
+
+
+// let cart = ["soap","ice-cream","facewash",20,null];
+
+// console.log(cart[2]);
+
+
+
+function addTextToTitle(myId){ // argument
+    let titleText =  "<p> We are Learning Javascript Basic </p>"
+    document.getElementById(myId).innerHTML = titleText;
+}
+
+addTextToTitle("title"); // parameter
+ 
+addTextToTitle("titletwo");
+
+addTextToTitle("titlethree");
