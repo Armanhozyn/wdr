1a44ec02efbca2c4c807bc2b9d1ee12a55b9ea16
wellupweb <wellupweb@gmail.com>
2023-07-19 13:04:17 +0600

    hello

--- javascript/index.js
+++ javascript/index.js
@@ -17,12 +17,63 @@
 
 // document.getElementById('title').innerHTML = logo; 
 
-var email = prompt("Enter your email");
-if(email == 'utfola@gmail.com'){
-    var message = "Thanks for login !!";
-    alert(message);
-}else{
-    var message = "You are not a valid user!!";
-    alert(message);
+// var email = prompt("Enter your email");
+// var password = prompt("Enter your password");
+// if(email == 'utfola@gmail.com' && password == "1234"){
+//     var message = "Thanks for login !!";
+//     alert(message);
+// }else{
+//     var message = "You are not a valid user!!";
+//     alert(message);
+// } 
+
+// var num1 = prompt("Enter the number 1");
+// var num2 = prompt("Enter the number 2");
+
+// var sum = parseInt(num1) * parseInt(num2);
+
+// alert(`The sum is ${sum}`);
+
+// var country_name1 = "Bangladesh";
+// document.getElementById('titletwo').innerHTML = `I love my country ${country_name1}`;
+
+// var country_name2 = "Sweden";
+// document.getElementById('title').innerHTML = `I love my country ${country_name2}`;
+
+
+// function myCountryName(countryName,elementName){
+//     document.getElementById(elementName).innerHTML = `I love my country ${countryName}`;
+// }
+// myCountryName("Bangladesh","title");
+// myCountryName("Sweden","titletwo");
+// myCountryName("Argentina","titlethreee");
+
+// mone kori
+
+var total_post = 100;
+
+var num = 0
+
+while (num < total_post) {
+    num = num + 2;
+    console.log(num);
 }
-  
+
+
+var country = ['brazil','argentina','usa'];
+console.log(country);
+
+country.push('canada');
+console.log(country)
+
+country.unshift();
+country.pop();
+country.shift();
+console.log(country)
+
+
+
+
+
+
+
